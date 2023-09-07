4fb19e4f9415752ab657709092f776f8e4cafe71
Rabsterd <ksm6798@gmail.com>
2023-09-07 17:46:38 +0900

    eleventh commit

---
+++ 03_Javascript/js/펭귄폭탄마게임.js
@@ -0,0 +1,15 @@
+
+
+map.before(penguin);
+document.getElementById("penguin").addEventListener("keydown", function() {
+
+    const map = document.createElement("penguin");
+    const penguin = document.getElementById("penguin");
+    const boom = document.createElement("boom");
+    const boom2 = document.createElement("boom2");
+
+
+
+    map.appendChild("penguin");
+
+}) 
