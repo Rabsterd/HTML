4b964ac24e814cf49f9d211f14f0675c69dab54d
Rabsterd <ksm6798@gmail.com>
2023-09-08 17:11:20 +0900

    12th commit

--- 03_Javascript/js/펭귄폭탄마게임.js
+++ 03_Javascript/js/펭귄폭탄마게임.js
@@ -1,15 +1,37 @@
+let xindex = 0;
+let yindex = 0;
 
 
-map.before(penguin);
-document.getElementById("penguin").addEventListener("keydown", function() {
+document.addEventListener("keydown", function(e) {
 
-    const map = document.createElement("penguin");
-    const penguin = document.getElementById("penguin");
-    const boom = document.createElement("boom");
-    const boom2 = document.createElement("boom2");
+console.log("누르는 중" + e.key);
 
+const penguin = document.getElementById("penguin");
+const boom = document.createElement("img");
 
+if(e.key == "ArrowRight") {
+    xindex += 10;
+    penguin.style.transform= 'translate3d(${xindex}px, (${yindex}px, 0)';
+} else if(e.key == "ArrowLeft") {
+    xindex -= 10;
+    penguin.style.transform= 'translate3d(${xindex}px, (${yindex}px, 0)';
+} else if(e.key == "ArrowDown") {
+    yindex += 10;
+    penguin.style.transform= 'translate3d(${xindex}px, (${yindex}px, 0)';
+} else if(e.key == "ArrowUp") {
+    yindex -= 10;
+    penguin.style.transform= 'translate3d(${xindex}px, (${yindex}px, 0)';
+} else if(e.key == "x") {
 
-    map.appendChild("penguin");
+    const box = document.getElementById("box");
+    boom.setAttribute("src", "/Image/drive-download-20230907T064740Z-001/boom.png");
+    boom.style.transform = 'translate3d(${xindex}px, ${yindex}px, 0';
+    boom.style.position = "absolute";
+    box.append(boom);
+}
+
+setTimeout(function() {
+    boom.setAttribute("src", "/Image/drive-download-20230907T064740Z-001/boom2.png");
+}, 2000);
 
 }) 
